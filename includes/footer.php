cbe3e7915d0d50021f253cf5a43fabdba383383b
kimha7 <hakimkiggundu@gmail.com>
2016-11-15 01:58:22 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <td><hr style="height:10px; background-color:#999; margin-top:4px; padding:0px; border:0px" /></td>
   </tr>
   <tr height="" style="background-color:#E1E1FF">
-  <td><p align="center" style="font:'Courier New', Courier, monospace">All rights reserved. National Water And Sewerage Coperation</p></td>
+  <td><p align="center" style="font:'Courier New', Courier, monospace">All rights reserved. </p></td>
   </tr>
 </table>
 
